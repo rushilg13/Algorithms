32c5bfc4085b51df1e0f69961ba89e61e85768de
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2020-11-24 19:55:10 +0530

    Update Selection.py

--- Selection Sort/Selection Sort in Python/Selection.py
+++ Selection Sort/Selection Sort in Python/Selection.py
@@ -8,3 +8,22 @@ for i in range(len(nums)):
     nums[i], nums[ind] = nums[ind], nums[i]
         
 print(nums)
+
+
+
+# APPROACH - 2
+
+print("Enter numbers in list (Space seperated):")
+A = list(map(int, input().split()))
+for i in range(len(A)): 
+ 
+    min_idx = i 
+    for j in range(i+1, len(A)): 
+        if A[min_idx] > A[j]: 
+            min_idx = j 
+   
+    A[i], A[min_idx] = A[min_idx], A[i] 
+  
+print ("Sorted array:") 
+for i in range(len(A)): 
+    print(A[i], end=' ') 

37cc5ce9407ff70c36108ad779b1a31fa55989a9
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 06:24:38 +0530

    Added Class Node

--- Dijkstra Algorithm/Dijkstra Algorithm in Python/Dijkstra.py
+++ Dijkstra Algorithm/Dijkstra Algorithm in Python/Dijkstra.py
@@ -7,3 +7,18 @@ class edge:
         self.start = start
         self.target = target
 
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.visited = False
+        self.predecessor = None
+        self.adjacent = []
+        self.minDist = sys.maxsize
+    
+    def __cmp__(self,other):
+        return self.cmp(self.minDist, other.minDist)
+
+    def __it__(self, other):
+        selfPriority = self.minDist
+        otherPriority = self.minDist
+        return selfPriority < otherPriority
